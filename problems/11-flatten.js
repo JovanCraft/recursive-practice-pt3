3f9a595f776b0fe826832e8cdeb9ffb53bbe0280
jovan <jovancraft@gmail.com>
2022-10-11 18:30:58 -0500

    worked on flattened

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -14,8 +14,16 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 
 function flatten(arr) {
   // Your code here
+  if (arr.length === 0){
+    return [];
+  }
+  let firstEl = arr[0]
+  arr.shift()
+  return firstEl.concat(flatten(arr))
 }
-  
+
+//flatten([]); // []
+//flatten([1, 2]); // [1, 2]
+flatten([1, [2, [3]]]); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
