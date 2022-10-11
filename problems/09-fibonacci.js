495ef8145ee2493127a6a8d60e32abd6f01e7c48
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-11 15:57:23 -0700

    did problem 9

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -5,10 +5,19 @@ and returns the `n`th number in the Fibonacci sequence.
 Not familiar with the Fibonacci sequence? Beginning with 0 and 1, we add the two
 previous numbers in the sequence together to form the next one:
 
-0, 1, 1, 2, 3, 5, 8, etc....
+  1st digit
+0, [1], 1, 2, 3, 5, 8, etc....
 
 We count Fibonacci numbers beginning with the first 1. Take a look at the
 examples below if you're unsure where to start!
+
+take first 2 numbers and add them, keep doing until we've done it n times
+
+1.
+2.
+3.
+4.
+5.
 
 Examples:
 
@@ -21,8 +30,15 @@ fibonacci(10); // 55
 
 function fibonacci(n) {
   // Your code here
+
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  debugger;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
-  
+
+fibonacci(5); // 5
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = fibonacci;
-  
