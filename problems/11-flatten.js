a75382937af677e8813e3755c26b7173b1f16a25
jovan <jovancraft@gmail.com>
2022-10-12 15:41:06 -0500

    finished pt3

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -5,6 +5,23 @@ contents on one level.
 
 Do not use the built in Array.flat.
 
+if (arr.length === 0){
+    return [];
+  } else if (arr.length === 1){
+    return arr[0]
+  }
+  if (Array.isArray(arr[0])){
+    var newArr = arr[0]
+    arr.shift()
+  } else {
+    var newArr = [arr[0]];
+    arr.shift()
+  }
+  if (Array.isArray(arr[0])){
+    return newArr.concat(flatten(arr[0]))
+  } else {
+    return newArr.concat(flatten(arr))
+  }
 Examples:
 
 flatten([]); // []
@@ -13,17 +30,25 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 function flatten(arr) {
-  // Your code here
-  if (arr.length === 0){
-    return [];
+  //Your code here
+  const flat = [];
+
+    arr.forEach(ele => {
+      if (Array.isArray(ele)) {
+        flat.push(...flatten(ele));
+      } else {
+        flat.push(ele);
+      }
+    });
+
+    return flat;
   }
-  let firstEl = arr[0]
-  arr.shift()
-  return firstEl.concat(flatten(arr))
-}
+
+
 
 //flatten([]); // []
 //flatten([1, 2]); // [1, 2]
-flatten([1, [2, [3]]]); // [1, 2, 3]
+flatten([1, [2, [3]]]); // [1, 2, 3]//fix 3rd one!!!
+flatten([1, [2, [3],5]])//
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -13,9 +13,24 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 function isSorted(arr) {
   // Your code here
+  if (arr.length === 0){
+    throw "This Array is empty error"
+  }
+  if (arr.length === 1){
+    return true
+  }
+  if (arr[0] >= arr[1]){
+    return false
+  }
+  arr.shift()
+  return isSorted(arr)
 }
 
-
+isSorted([1, 2, 3, 4, 5]); // true
+isSorted([1, 2, 4, 3, 5]); // false
+isSorted([2, 4, 6, 7, 8]); // true
+isSorted([5, 4, 3, 2, 1]); // false
+isSorted([])
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = isSorted;
